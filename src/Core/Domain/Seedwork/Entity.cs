86fdba5c20e86f3da1a315a72011307ee0816a3a
Dariush Tasdighi <DariushT@GMail.com>
2023-10-22 23:23:38 +0330

    Add project files.

---
+++ src/Core/Domain/Seedwork/Entity.cs
@@ -0,0 +1,46 @@
+﻿namespace Domain.Seedwork;
+
+public abstract class Entity : object,
+	Dtat.Seedwork.Abstractions.IEntity<System.Guid>
+{
+	#region Constructor
+	public Entity() : base()
+	{
+		Id = System.Guid.NewGuid();
+
+		InsertDateTime = Dtat.DateTime.Now;
+	}
+	#endregion /Constructor
+
+	#region Properties
+
+	#region public System.Guid Id { get; protected set; }
+	/// <summary>
+	/// شناسه
+	/// </summary>
+	[System.ComponentModel.DataAnnotations.Display
+		(ResourceType = typeof(Resources.DataDictionary),
+		Name = nameof(Resources.DataDictionary.Id))]
+
+	[System.ComponentModel.DataAnnotations.Key]
+
+	[System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated
+		(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None)]
+	public System.Guid Id { get; protected set; }
+	#endregion /public System.Guid Id { get; protected set; }
+
+	#region public System.DateTimeOffset InsertDateTime { get; private set; }
+	/// <summary>
+	/// زمان ایجاد
+	/// </summary>
+	[System.ComponentModel.DataAnnotations.Display
+		(ResourceType = typeof(Resources.DataDictionary),
+		Name = nameof(Resources.DataDictionary.InsertDateTime))]
+
+	[System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated
+		(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None)]
+	public System.DateTimeOffset InsertDateTime { get; private set; }
+	#endregion /public System.DateTimeOffset InsertDateTime { get; private set; }
+
+	#endregion /Properties
+}
---
+++ src/Framework/Dtat.Seedwork.Abstractions/IEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Dtat.Seedwork.Abstractions;
+
+public interface IEntity<TIdentity>
+{
+	public TIdentity Id { get; }
+
+	public System.DateTimeOffset InsertDateTime { get; }
+}
